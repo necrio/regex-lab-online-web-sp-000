7b80989e75a72c0fa2c314bb7e81f083e84f1b5b
Keith Isham <kisham8899@gmail.com>
2019-04-20 17:10:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -41,7 +41,7 @@ end
 def valid_phone_number?(phone)
 
   # if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) then\
-    if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) then
+    if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) 
   return true
 else
   return false
